b5138b0b45399feb8b8fc3391f25860b81c9ffe1
Adil Chakkala Paramba <adil@careberri.com>
2023-01-14 17:18:31 +0100

    created custom hook controller for bar code reader

---
+++ lib/core/hooks/camera_controller_hook.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
+
+MobileScannerController useMobileScannerController({
+  required bool torchEnabled,
+  required CameraFacing cameraFacing,
+}) {
+  return use(
+    _MobileScannerControllerHook(
+        torchEnabled: torchEnabled, cameraFacing: cameraFacing),
+  );
+}
+
+class _MobileScannerControllerHook extends Hook<MobileScannerController> {
+  const _MobileScannerControllerHook({
+    required this.torchEnabled,
+    required this.cameraFacing,
+  });
+
+  final bool torchEnabled;
+  final CameraFacing cameraFacing;
+
+  @override
+  HookState<MobileScannerController, Hook<MobileScannerController>>
+      createState() => _MobileScannerControllerHookState();
+}
+
+class _MobileScannerControllerHookState
+    extends HookState<MobileScannerController, _MobileScannerControllerHook> {
+  late final MobileScannerController _controller;
+
+  @override
+  void initHook() {
+    super.initHook();
+    _controller = MobileScannerController(
+      facing: hook.cameraFacing,
+      torchEnabled: hook.torchEnabled,
+      detectionSpeed: DetectionSpeed.noDuplicates,
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  MobileScannerController build(BuildContext context) => _controller;
+
+  @override
+  String get debugLabel => 'useMobileScannerController';
+}
